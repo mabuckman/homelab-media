7033c12a8e326514c5cdff9961a2f4716dd292fe
mabuckman <mbuck25@gmail.com>
2019-08-26 14:36:50 -0500

    Ensure directories exist

--- install.sh
+++ install.sh
@@ -2,6 +2,16 @@
 
 cp *.service env.list /etc/systemd/system
 
+mkdir -p ~/media/movies
+mkdir -p ~/media/tv
+mkdir -p ~/media/youtube
+
+mkdir -p ~/jacket/config
+mkdir -p ~/jacket/downloads
+
+mkdir -p ~/radarr/config
+mkdir -p ~/media/movies
+
 systemctl daemon-reload
 systemctl enable docker-jackett.service
 systemctl enable docker-radarr.service
