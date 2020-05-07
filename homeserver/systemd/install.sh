402855b5b12c300e9b5646413ccd80243e10bf01
Matthew Buckman <mbuck25@gmail.com>
2020-05-07 11:54:05 +0000

    Add service to automatically update docker containers

--- homeserver/systemd/install.sh
+++ homeserver/systemd/install.sh
@@ -12,8 +12,9 @@ systemctl enable docker-jackett.service
 systemctl enable docker-radarr.service
 systemctl enable docker-nzbhydra2.service
 systemctl enable docker-sonarr.service
+systemctl enable docker-watchtower.service
 
 systemctl start docker-jackett.service
 systemctl start docker-radarr.service
 systemctl start docker-nzbhydra2.service
-systemctl start docker-sonarr.service
+systemctl start docker-watchtower.service
